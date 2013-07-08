2fa6df6fe67e89d9f65f9e9641ae18c67651a8c6
tao.xu <xutaosjtu@gmail.com>
2013-07-08 21:24:25 +0200

    minor change: add inversion counting

--- Assignment1.R
+++ Assignment1.R
@@ -10,46 +10,44 @@ swap <-function(x){
 }
 
 combine <- function(x,y){
+  count = 0
   z = NULL
-  k=1;j=1;
-  tmp1 = x[k]
-  tmp2 = y[j]
-  while(k<=length(x) & j<=length(y)){
-    if(tmp1<tmp2){
-      z = c(z,tmp1)
-      k = k +1
-      tmp1 = x[k]
+  i=1;j=1;
+  #tmp1 = x[k]
+  #tmp2 = y[j]
+  while(i<=length(x) & j<=length(y)){
+    if(x[i]<y[j]){
+      z = c(z,x[i])
+      i = i +1
     }
     else{
-      z = c(z,tmp2)
+      z = c(z,y[j])
       j = j+1
-      tmp2 = y[j]
+      count = count + length(x)-i+1
     }
   }
-  
-  if(k>length(x)){
-    return(z = c(z,y[j:length(y)]))
+  #print(count)
+  if(i>length(x)){
+    z = c(z,y[j:length(y)])
   }
-  else return(z = c(z,x[k:length(x)]))
+  else z = c(z,x[i:length(x)])
   
+  return(list(count = count, z= z))
 }
 
 inverse<- function(A, n){
   if(n==1){
-    #print("A = ");
-    #print(A)
-    return(A);
+    return(list(count = 0, z = A));
   }
   else{
     new_n = round(n/2)
     x = inverse(A[1:new_n],new_n)
     y = inverse(A[(new_n+1):n],(n-new_n))
-    z = combine(x,y)
+    z = combine(x[[2]],y[[2]])
+    z[[1]] = z[[1]]+x[[1]]+y[[1]]
     return(z)
   }
 }
 
-
-f = function(x){
-  return(c(x*x,x+2))
-}
+count=0
+inverse(a,6)
